123fc42a59fe73a6e39177ea91e405ebdacb90f4
StevePro7 <steveprozune@hotmail.com>
2020-08-31 20:58:30 +0100

    no message

---
+++ Flicky.asm
@@ -0,0 +1,190 @@
+; This disassembly was created using Emulicious (http://www.emulicious.net)
+.MEMORYMAP	
+SLOTSIZE $4000	
+SLOT 0 $0000	
+SLOT 1 $4000	
+SLOT 2 $8000	
+DEFAULTSLOT 2	
+.ENDME	
+.ROMBANKMAP	
+BANKSTOTAL 2	
+BANKSIZE $4000	
+BANKS 2
+.ENDRO	
+	
+.enum $C000 export	
+_RAM_C000_ db	
+.ende	
+	
+.enum $C0C9 export	
+_RAM_C0C9_ db	
+_RAM_C0CA_ db	
+_RAM_C0CB_ db	
+_RAM_C0CC_ db	
+_RAM_C0CD_ db	
+_RAM_C0CE_ db	
+_RAM_C0CF_ dsb $8	
+_RAM_C0D7_ db	
+_RAM_C0D8_ db	
+.ende	
+	
+.enum $C0DA export	
+_RAM_C0DA_ db	
+.ende	
+	
+.enum $C0DC export	
+_RAM_C0DC_ db	
+.ende	
+	
+.enum $C0DE export	
+_RAM_C0DE_ db	
+_RAM_C0DF_ db	
+_RAM_C0E0_ db	
+_RAM_C0E1_ db	
+.ende	
+	
+.enum $C0E3 export	
+_RAM_C0E3_ dw	
+_RAM_C0E5_ dw	
+_RAM_C0E7_ db	
+_RAM_C0E8_ db	
+_RAM_C0E9_ db	
+_RAM_C0EA_ db	
+_RAM_C0EB_ db	
+_RAM_C0EC_ db	
+_RAM_C0ED_ db	
+_RAM_C0EE_ db	
+_RAM_C0EF_ db	
+_RAM_C0F0_ db	
+_RAM_C0F1_ dw	
+_RAM_C0F3_ dsb $4	
+_RAM_C0F7_ dw	
+_RAM_C0F9_ db	
+_RAM_C0FA_ db	
+_RAM_C0FB_ dsb $3	
+_RAM_C0FE_ db	
+_RAM_C0FF_ db	
+_RAM_C100_ db	
+_RAM_C101_ db	
+_RAM_C102_ db	
+_RAM_C103_ db	
+.ende	
+	
+.enum $C107 export	
+_RAM_C107_ db	
+_RAM_C108_ db	
+_RAM_C109_ db	
+_RAM_C10A_ db	
+_RAM_C10B_ db	
+_RAM_C10C_ db	
+_RAM_C10D_ db	
+_RAM_C10E_ db	
+_RAM_C10F_ db	
+_RAM_C110_ db	
+_RAM_C111_ db	
+_RAM_C112_ db	
+_RAM_C113_ db	
+_RAM_C114_ db	
+_RAM_C115_ db	
+_RAM_C116_ db	
+_RAM_C117_ db	
+_RAM_C118_ db	
+.ende	
+	
+.enum $C11A export	
+_RAM_C11A_ db	
+.ende	
+	
+.enum $C11C export	
+_RAM_C11C_ db	
+.ende	
+	
+.enum $C125 export	
+_RAM_C125_ db	
+.ende	
+	
+.enum $C13D export	
+_RAM_C13D_ db	
+_RAM_C13E_ db	
+_RAM_C13F_ db	
+_RAM_C140_ db	
+.ende	
+	
+.enum $C147 export	
+_RAM_C147_ db	
+_RAM_C148_ dsb $7	
+_RAM_C14F_ db	
+_RAM_C150_ db	
+_RAM_C151_ db	
+_RAM_C152_ db	
+_RAM_C153_ db	
+_RAM_C154_ db	
+_RAM_C155_ db	
+_RAM_C156_ db	
+_RAM_C157_ db	
+_RAM_C158_ dw	
+_RAM_C15A_ db	
+.ende	
+	
+.enum $C15C export	
+_RAM_C15C_ dsb $6	
+_RAM_C162_ db	
+.ende	
+	
+.enum $C164 export	
+_RAM_C164_ db	
+.ende	
+	
+.enum $C166 export	
+_RAM_C166_ dsb $8	
+.ende	
+	
+.enum $C176 export	
+_RAM_C176_ db	
+.ende	
+	
+.enum $C182 export	
+_RAM_C182_ dsb $18	
+_RAM_C19A_ db	
+_RAM_C19B_ db	
+_RAM_C19C_ db	
+.ende	
+	
+.enum $C1A1 export	
+_RAM_C1A1_ db	
+_RAM_C1A2_ db	
+.ende	
+	
+.enum $C1A5 export	
+_RAM_C1A5_ dsb $4	
+_RAM_C1A9_ db	
+_RAM_C1AA_ db	
+.ende	
+	
+.enum $C1B1 export	
+_RAM_C1B1_ db	
+.ende	
+	
+.enum $C1B9 export	
+_RAM_C1B9_ db	
+.ende	
+	
+.enum $C1D9 export	
+_RAM_C1D9_ db	
+.ende	
+	
+.enum $C1F1 export	
+_RAM_C1F1_ dsb $10	
+.ende	
+	
+.enum $C320 export	
+_RAM_C320_ db	
+.ende	
+	
+.enum $C324 export	
+_RAM_C324_ dsb $a8	
+.ende	
+	
+.enum $E041 export	
+_RAM_E041_ dw	
+.ende	
