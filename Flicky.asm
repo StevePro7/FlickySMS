e6ff2629510fe025f43d4db6f9cbb5600d4e5d0f
StevePro7 <steveprozune@hotmail.com>
2020-08-31 21:32:45 +0100

    no message

--- Flicky.asm
+++ Flicky.asm
@@ -187,4 +187,195 @@ _RAM_C324_ dsb $a8
 	
 .enum $E041 export	
 _RAM_E041_ dw	
-.ende	
+.ende
+
+; Ports	
+.define Port_PSG $7F	
+.define Port_VDPData $BE	
+.define Port_VDPAddress $BF	
+.define _PORT_DE_ $DE	
+.define _PORT_DF_ $DF	
+
+; Input Ports
+.define Port_VDPStatus $BF
+.define Port_IOPort1 $DC
+
+.BANK 0 SLOT 0
+.ORG $0000
+
+; no$gmb symbolic information for "output.sms".	
+_START:
+    di
+    ld sp, $C0C8
+    im 1
+    call _LABEL_7395_2
+    ld hl, _RAM_C000_
+    ld bc, $0400
+_LABEL_F_3:
+    ld (hl), $00
+    inc hl
+    dec bc
+    ld a, c
+    jr nz, _LABEL_F_3
+    call _LABEL_2884_4
+    jr _LABEL_70_7
+    
+_LABEL_1C_11:
+    in a, (Port_VDPStatus)
+    ld b, $0E
+    ld hl, _DATA_2A_
+    
+    
+; Data from 2A to 37 (14 bytes)	
+_DATA_2A_:	
+	.db $02 $80 $82 $81 $0E $82 $FF $83 $03 $84 $76 $85 $03 $86
+    
+    
+        
+_stevepro:
+    jp _stevepro
+    
+_LABEL_70_7:
+    ld b, $0A
+    call _LABEL_1B3_8
+    call _LABEL_1C_11
+    call _LABEL_1BF_13
+    
+
+_LABEL_1B3_8:
+    ld de, $FFFF
+_LABEL_1B6_10:	
+    ld hl, $39DE
+_LABEL_1B9_9:
+    add hl, de
+    jr c, _LABEL_1B9_9    
+    djnz _LABEL_1B6_10
+    ret
+
+_LABEL_7395_2:	
+    ld hl, _DATA_739F_
+    ld c, Port_PSG
+    ld b, $04
+    otir
+    ret
+    
+; Data from 739F to 7434 (150 bytes)	
+_DATA_739F_:	
+	.db $9F $BF $DF $FF $00 $00 $FF $03 $C7 $03 $90 $03 $5D $03 $2D $03
+	.db $FF $02 $D4 $02 $AB $02 $85 $02 $61 $02 $3F $02 $1E $02 $00 $02
+	.db $E3 $01 $C8 $01 $AF $01 $96 $01 $80 $01 $6A $01 $56 $01 $43 $01
+	.db $30 $01 $1F $01 $0F $01 $00 $01 $F2 $00 $E4 $00 $D7 $00 $CB $00
+	.db $C0 $00 $B5 $00 $AB $00 $A1 $00 $98 $00 $90 $00 $88 $00 $80 $00
+	.db $79 $00 $72 $00 $6C $00 $66 $00 $60 $00 $5B $00 $55 $00 $51 $00
+	.db $4C $00 $48 $00 $44 $00 $40 $00 $3C $00 $39 $00 $36 $00 $33 $00
+	.db $30 $00 $2D $00 $2B $00 $28 $00 $26 $00 $24 $00 $22 $00 $20 $00
+	.db $1E $00 $1C $00 $1B $00 $19 $00 $18 $00 $16 $00 $15 $00 $14 $00
+	.db $13 $00 $12 $00 $11 $00    
+    
+    
+_LABEL_2884_4:
+ld a, $92
+    out (_PORT_DF_), a
+    ld a, $55
+    out (_PORT_DE_), a
+    in a, (_PORT_DE_)
+    cp $55
+    ld c, $00
+    jr z, _LABEL_2896_5
+    ld c, $FF
+_LABEL_2896_5:
+    ld a, $AA
+    out (_PORT_DE_), a
+    in a, (_PORT_DE_)
+    cp $AA
+    ld a, $00
+    jr z, _LABEL_28A4_6
+    ld a, $FF
+_LABEL_28A4_6:
+    or c
+    ld (_RAM_C0CB_), a
+    ld a, $07
+    out (_PORT_DE_), a
+    ret
+    
+_LABEL_E5_14:
+    push bc
+    push af
+    call _LABEL_F6_15
+_LABEL_EA_16:
+    pop af
+    out (Port_VDPData), a
+    push af
+    dec bc
+    ld a, b
+    or c
+    jr nz, _LABEL_EA_16
+    pop af
+    pop bc
+    ret
+    
+_LABEL_F6_15:
+    ld a, l
+    out (Port_VDPAddress), a
+    ld a, h
+    or $40
+    out (Port_VDPAddress), a
+    ret
+    
+_LABEL_FF_97:	
+    ld a, l
+    out (Port_VDPAddress), a
+    ld a, h
+    out (Port_VDPAddress), a
+    ret
+        
+    
+_LABEL_1BF_13:
+    ld hl, $3800    
+    ld bc, $0300
+    ld a, $20
+    call _LABEL_E5_14
+    ld hl, _RAM_C0EC_
+    bit 5, (hl)
+    ret nz
+    ld de, _RAM_C1A1_
+    ld b, $20
+_LABEL_1D5_17:
+    push bc
+    ld hl, _DATA_1E5_
+    ld bc, $0004
+    ldir
+    pop bc
+    djnz _LABEL_1D5_17
+    call _LABEL_165_18
+    ret
+    
+; Data from 1E5 to 1E8 (4 bytes)	
+_DATA_1E5_:	
+	.db $F0 $00 $00 $00    
+    
+    
+_LABEL_106_19:
+    di
+    call _LABEL_F6_15
+    push de
+    push bc
+_LABEL_10C_20:
+    ld a, (de)
+    out (Port_VDPData), a
+    inc de
+    dec bc
+    ld a, c
+    or b
+    jr nz, _LABEL_10C_20
+    pop bc
+    pop de
+    ret
+    
+_LABEL_165_18:	
+    di
+    ld hl, $3B00
+    ld de, _RAM_C1A1_
+    ld bc, $0080
+    call _LABEL_106_19
+    ret
