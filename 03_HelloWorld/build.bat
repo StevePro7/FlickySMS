eb844a6796046fad299b32dc8c95437e9eac7de1
StevePro7 <steveprozune@hotmail.com>
2020-12-12 14:10:53 +0000

    no message

--- 03_HelloWorld/build.bat
+++ 03_HelloWorld/build.bat
@@ -1,11 +1,13 @@
 cls
 
-wla-z80.exe -o main.asm main.o
+::wla-z80.exe -o main.asm main.o
+wla-z80 main.asm
 
 ::echo [objects] > linkfile
 ::echo main.o >> linkfile
 
-wlalink -drvs linkfile output.sms
+::wlalink -drvs linkfile output.sms
+wlalink linkfile output.sms
 
-java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
-::output.sms
+::java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
+output.sms
